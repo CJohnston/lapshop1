29f60ed690baaebff11da21bf9f275c7f68caca0
Colin <colinjackson23@gmail.com>
2011-11-10 10:31:45 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lapshop1::Application.config.secret_token = '334287d7e44024ec426ee6f3734b5b190f739f15c92546d8a3edccfa796b7a05ef9ae92bb4e2667a567670cf1a42962121e45dba36ba004dceb529455d9a2489'
